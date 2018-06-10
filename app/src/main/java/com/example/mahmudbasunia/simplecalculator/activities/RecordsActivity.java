248c0e2578cea1179965cba418a6bb504bd095cc
Mahmud Basunia <mahmud.basunia@gmail.com>
2018-06-11 00:16:39 +0600

    no messages

---
+++ app/src/main/java/com/example/mahmudbasunia/simplecalculator/activities/RecordsActivity.java
@@ -0,0 +1,17 @@
+package com.example.mahmudbasunia.simplecalculator.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Mahmud Basunia on 6/11/2018.
+ */
+
+public class RecordsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
