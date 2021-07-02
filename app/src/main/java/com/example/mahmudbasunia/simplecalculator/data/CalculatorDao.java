3b5b943d743db2695a8dca80d265c2ae1ab4f063
Mahmud Basunia <mahmud.basunia@gmail.com>
2021-07-02 12:07:09 +0600

    migrated to adroidx

--- app/src/main/java/com/example/mahmudbasunia/simplecalculator/data/CalculatorDao.java
+++ app/src/main/java/com/example/mahmudbasunia/simplecalculator/data/CalculatorDao.java
@@ -1,14 +1,13 @@
 package com.example.mahmudbasunia.simplecalculator.data;
 
-import android.arch.persistence.room.Dao;
-import android.arch.persistence.room.Insert;
-import android.arch.persistence.room.Query;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
 
 import com.example.mahmudbasunia.simplecalculator.data.model.CalculationModel;
 
 import java.util.List;
 
-import io.reactivex.Completable;
 import io.reactivex.Single;
 
 /**
